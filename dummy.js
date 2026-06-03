778d5e38b5d2eab9a046e7d208de27c13bcd62b7
Vamshi0248 <vamsi.magam@clarista.io>
2026-06-03 12:25:01 +0530

    Add dummy code to feature/navigation branch

---
+++ dummy.js
@@ -0,0 +1,10 @@
+// Dummy code for feature/navigation branch
+function createNavigation() {
+  console.log("Creating navigation components...");
+  const navItems = ['Home', 'About', 'Services', 'Contact', 'Blog'];
+  navItems.forEach(item => {
+    console.log(`Adding navigation item: ${item}`);
+  });
+}
+
+createNavigation();
